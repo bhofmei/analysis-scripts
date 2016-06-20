40a4c4e85e360b67e9f01b47da6721dfb107197d
Brigitte Hofmeister <bhofmei@gmail.com>
2016-06-20 07:45:39 -0400

    update parese methyl pipeline to use command line options

--- parse_output/parse_pipeline_output.py
+++ parse_output/parse_pipeline_output.py
@@ -1,9 +1,119 @@
 import sys, os, glob, math
 
-# Usage: python3.4 parse_pipeline_output.py <path_to_run_output> <path_to_correction_output>
+# Usage: python parse_pipeline_output.py [-f=fasta_file | fasta_index] [-o=out_id] [-u=underscore] <path_to_run_output> <path_to_correction_output>
+# parse run/script information from output/error files
 
-UNDERSCORE=3
+UNDERSCORE=1
 
+def processInputs( pathRun, pathCorrection, fastaFileStr, outID, underScores ):
+	uPathRun = checkFolder( pathRun )
+	uPathCorrection = checkFolder( pathCorrection )
+	if uPathRun == False or uPathCorrection == False:
+		print( 'Error: folder {:s} or {:s} not found'.format( pathRun, pathCorrection ) )
+	totalDict = {}
+	runFiles = glob.glob(uPathRun+'*.py.o*')
+	errorFiles = glob.glob(uPathRun+'*.py.e*')
+	corFiles = glob.glob(uPathCorrection+'*.sh.*')
+	
+	cov = -1
+	if fastaFileStr != None:
+		print( 'Reading', fastaFileStr )
+		if fastaFileStr.endswith( '.fai' ):
+			cov = readIndex( fastaIndexStr )	
+		elif fastaFileStr != None:
+			cov = readFasta( fastaFileStr )
+	print( 'Reading run files for...')
+	for run in runFiles:
+		n = getSampleName( run, underScores+1 )
+		print( '-',n)
+		if totalDict.get(n) == None:
+			tempAr = readRunOutput( run )
+			totalDict[n]=tempAr
+		else:
+			print( 'ERROR: duplicate run files for {:s}, skipping {:s}'.format(n, run) )
+	print( 'Reading error files for...' )
+	for err in errorFiles:
+		n = getSampleName( err, underScores+1 )
+		print( '-',n)
+		if totalDict.get(n) == None:
+			print( 'ERROR: {:s} has error file but no run file'.format( n ) )
+		else:
+			tAr = totalDict.get(n)
+			tmpAr = readRunError( err )
+			tmpAr2 = tAr[0:4] + tmpAr + tAr[4:]
+			totalDict[n] = tmpAr2
+	print( 'Reading correction files for...' )
+	for cor in corFiles:
+		n = getSampleName( cor, underscore )
+		print('-',n )
+		if totalDict.get(n) == None:
+			print( 'ERROR: {:s} has correction file but not run file'.format(n ))
+		else:
+			tAr = totalDict.get(n)
+			nonConversion, tempDict = readCorrentionOutput( cor )
+			#print( tempDict )
+			#if tAr[-1] == -1:
+				#tAr[-1] = nonConversion
+			totalDict[n] = ( tAr, tempDict )
+	#print( totalDict )
+	# write output
+	print( 'Writing output...')
+	outFileStr = 'run_correction_output{:s}.csv'.format( '_'+outID if outID != None else '' )
+	writeOutput( totalDict, cov, outFileStr )
+	print( 'Done.' )
+
+def checkFolder( inputStr ):
+	if os.path.exists( inputStr ) == False:
+		return False
+	if inputStr.endswith( '/' ):
+		return inputStr
+	return inputStr + '/'
+	
+def readIndex( fastaIndexStr ):
+	baseCount = 0
+	indexFile = open( fastaIndexStr, 'r' )
+	for line in indexFile:
+		lineAr = line.rstrip().split('\t')
+		# (0) name (1) length (2) byte position (3) line length w/o \n
+		# (4) line length w/ \n
+		if lineAr[0] not in ['mitochondria','chloroplast']:
+			baseCount += int( lineAr[1] )
+	indexFile.close()
+	return baseCount
+
+def readFasta( fastaFileStr ):
+	baseCount = 0
+	fastaFile = open( fastaFileStr, 'r' )
+	include = False
+	for line in fastaFile:
+		line = line.rstrip()
+		# chromosome headers
+		if line.startswith('>'):
+			# check chloroplast/mitochondria
+			if line.startswith( '>chloroplast' ) or line.startswith( '>mitochondria'):
+				include = False
+			else:
+				include = True
+		# sequence to be included
+		elif include:
+			baseCount += len( line )
+	fastaFile.close()
+	return baseCount
+
+def getSampleName( fileStr, underScore ):
+	baseName = os.path.basename( fileStr )
+	baseName = baseName.replace( 'run_info_', '' )
+	fInd = 0
+	for i in range( underScore ):
+		tInd = baseName[fInd+1:].find( '_' )
+		if tInd != -1:
+			fInd += tInd + 1
+	if fInd ==0:
+		tInd = baseName.find( '.' )
+		return baseName[:tInd]
+	return baseName[:fInd]
+
+		
 def readRunOutput( runFileStr ):
 	# input_reads trimmed_reads x4 adapter_trimmed x4 quality_trimmed x4
 	# uniquely_mapped non_clonal % remaining non_conversion_rate
@@ -51,6 +161,24 @@ def readRunOutput( runFileStr ):
 	runFile.close()
 	return outAr
 
+def readRunError( runErrorFileStr ):
+	errorFile = open( runErrorFileStr, 'r' )
+	outAr = [ -1 ] * 2
+	# forward_one_alignment 
+	state = 0
+	
+	for line in errorFile:
+		line = line.rstrip()
+		if line.startswith( '# reads with at least one reported alignment' ):
+			lineAr = line.split(' ')
+			p = lineAr[-1].replace('(','').replace(')','').replace('%','')
+			outAr[state] = float( p )
+			state += 1
+		elif line.startswith( '[mpileup]' ):
+			break
+	errorFile.close()
+	return outAr
+
 def readCorrentionOutput( corFileStr ):
 	# lots-of cutoff and FDR
 	outDict = {}
@@ -77,50 +205,6 @@ def readCorrentionOutput( corFileStr ):
 	corFile.close()
 	return nonConversion, outDict
 
-def readFasta( fastaFileStr ):
-	baseCount = 0
-	fastaFile = open( fastaFileStr, 'r' )
-	include = False
-	for line in fastaFile:
-		line = line.rstrip()
-		# chromosome headers
-		if line.startswith('>'):
-			# check chloroplast/mitochondria
-			if line.startswith( '>chloroplast' ) or line.startswith( '>mitochondria'):
-				include = False
-			else:
-				include = True
-		# sequence to be included
-		elif include:
-			baseCount += len( line )
-	fastaFile.close()
-	return baseCount
-
-def readIndex( fastaIndexStr ):
-	baseCount = 0
-	indexFile = open( fastaIndexStr, 'r' )
-	for line in indexFile:
-		lineAr = line.rstrip().split('\t')
-		# (0) name (1) length (2) byte position (3) line length w/o \n
-		# (4) line length w/ \n
-		if lineAr[0] not in ['mitochondria','chloroplast']:
-			baseCount += int( lineAr[1] )
-	indexFile.close()
-	return baseCount
-
-def getSampleName( fileStr, underScore ):
-	baseName = os.path.basename( fileStr )
-	baseName = baseName.replace( 'run_info_', '' )
-	fInd = 0
-	for i in range( underScore ):
-		tInd = baseName[fInd+1:].find( '_' )
-		if tInd != -1:
-			fInd += tInd + 1
-	if fInd ==0:
-		tInd = baseName.find( '.' )
-		return baseName[:tInd]
-	return baseName[:fInd]
-
 def writeOutput( totalDict, cov, outFileStr ):
 	# totalDict[n]=(run_array,correction_dict)
 	outFile = open( outFileStr, 'w' )
@@ -138,7 +222,7 @@ def writeOutput( totalDict, cov, outFileStr ):
 		try:
 			ar = [ '{:d}'.format(x) for x in tup[0][0:4] ]
 			ar += [ '{:.2f}'.format(x) for x in tup[0][4:6] ]
-			ar += [ '{:d}'.format(x) for x in tup[0][6:8]]
+			ar += [ '{:g}'.format(x) for x in tup[0][6:8]]
 			ar += [ '{:.5f}'.format(x) for x in tup[0][8:] ]
 			if cov != -1:
 				# mappedBases = uniquely mapped reads * floor( processBases / processReads )
@@ -157,97 +241,46 @@ def writeOutput( totalDict, cov, outFileStr ):
 		outFile.write( outStr )
 	outFile.close()
 
-def readRunError( runErrorFileStr ):
-	errorFile = open( runErrorFileStr, 'r' )
-	outAr = [ -1 ] * 2
-	# forward_one_alignment 
-	state = 0
-	
-	for line in errorFile:
-		line = line.rstrip()
-		if line.startswith( '# reads with at least one reported alignment' ):
-			lineAr = line.split(' ')
-			p = lineAr[-1].replace('(','').replace(')','').replace('%','')
-			outAr[state] = float( p )
-			state += 1
-		elif line.startswith( '[mpileup]' ):
-			break
-	errorFile.close()
-	return outAr
 			
-
-def mainFunction( pathRun, pathCorrection, fastaFileStr, fastaIndexStr ):
-	underscore = UNDERSCORE
-	totalDict = {}
-	runFiles = glob.glob(pathRun+'*.py.o*')
-	errorFiles = glob.glob(pathRun+'*.py.e*')
-	corFiles = glob.glob(pathCorrection+'*.sh.*')
+def parseInputs( argv ):
+	outID = None
+	fastaFileStr = None
+	underScores = 0
+	startInd = 0
 	
-	cov = -1
-	if fastaFileStr != None:
-		cov = readFasta( fastaFileStr )
-		print( 'Read', fastaFileStr )
-	elif fastaIndexStr != None:
-		cov = readIndex( fastaIndexStr )
-		print( 'Read', fastaIndexStr )
-	print( 'Reading run files for...')
-	for run in runFiles:
-		n = getSampleName( run, underscore )
-		print( '-',n)
-		if totalDict.get(n) == None:
-			tempAr = readRunOutput( run )
-			totalDict[n]=tempAr
-			#print( tempAr )
-		else:
-			print( 'ERROR: duplicate run files for {:s}, skipping {:s}'.format(n, run) )
-	print( 'Reading error files for...' )
-	for err in errorFiles:
-		n = getSampleName( err, underscore )
-		print( '-',n)
-		if totalDict.get(n) == None:
-			print( 'ERROR: {:s} has error file but no run file'.format( n ) )
-		else:
-			tAr = totalDict.get(n)
-			tmpAr = readRunError( err )
-			tmpAr2 = tAr[0:4] + tmpAr + tAr[4:]
-			totalDict[n] = tmpAr2
-	print( 'Reading correction files for...' )
-	for cor in corFiles:
-		n = getSampleName( cor, underscore )
-		print('-',n )
-		if totalDict.get(n) == None:
-			print( 'ERROR: {:s} has correction file but not run file'.format(n ))
-		else:
-			tAr = totalDict.get(n)
-			nonConversion, tempDict = readCorrentionOutput( cor )
-			#print( tempDict )
-			#if tAr[-1] == -1:
-				#tAr[-1] = nonConversion
-			totalDict[n] = ( tAr, tempDict )
-	#print( totalDict )
-	# write output
-	print( 'Writing output...')
-	writeOutput( totalDict, cov, 'run_correction_output.csv' )
-	print( 'Done.' )
+	for i in range(min(3,len(argv)-2)):
+		if argv[i].startswith( '-o=' ):
+			outID = argv[i][3:]
+			startInd += 1
+		elif argv[i].startswith( '-f=' ):
+			fastaFileStr = argv[i][3:]
+			startInd += 1
+		elif argv[i].startswith( '-u=' ):
+			try:
+				underScores = int( argv[i][3:] )
+				startInd += 1
+			except ValueError:
+				print( 'ERROR: number of underscores must be an integer' )
+				exit()
+	# end for 
+	pathRun = argv[startInd]
+	pathCorrection = argv[startInd+1]
+	
+	processInputs( pathRun, pathCorrection, fastaFileStr, outID, underScores )
+
+def printHelp():
+	print ("Usage: python parse_pipeline_output.py [-f=fasta_file | fasta_index] [-o=out_id] [-u=underscore] <path_to_run_output> <path_to_correction_output>")
+	print( 'Required: ')
+	print( 'path_to_run_output\tpath to *.py.o* and *.py.e* files' )
+	print( 'path_to_correction_output\tpath to *.sh.o* files' )
+	print( 'Optional: ' )
+	print( '-f=fasta\tfasta genome file or fasta index[default none]\n\t\tused to compute coverage [' )
+	print( '-u=underscore_count\tunderscores is sample id name [default 0]' )
+	print( '-o=out_id\toutput identifier' )
 	
 
 if __name__ == "__main__":
 	if len(sys.argv) < 3 :
-		print ("Usage: python3.4 parse_pipeline_output.py [fasta_file | fasta_index] <path_to_run_output> <path_to_correction_output>")
+		printHelp()
 	else:
-		if sys.argv[1].endswith('.fa') or sys.argv[1].endswith('.fasta'):
-			fastaFileStr = sys.argv[1]
-			fastaIndexStr = None
-			pathRun = sys.argv[2]
-			pathCorrection = sys.argv[3]
-		elif sys.argv[1].endswith('.fai'):
-			fastaFileStr = None
-			fastaIndexStr = sys.argv[1]
-			pathRun = sys.argv[2]
-			pathCorrection = sys.argv[3]
-		else:
-			fastaFileStr = None
-			fastaIndexStr = None
-			pathRun = sys.argv[1]
-			pathCorrection = sys.argv[2]
-		mainFunction( pathRun, pathCorrection, fastaFileStr, fastaIndexStr )
+		parseInputs( sys.argv[1:] )
